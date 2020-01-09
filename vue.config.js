2c7710288e4f37b5210051bb85ca39434ed23233
Jeremie Pardou <571533+jrmi@users.noreply.github.com>
2020-01-09 20:24:16 +0100

    Change command

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,15 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   chainWebpack: config => {
     // GraphQL Loader
     config.module
-      .rule('ink')
+      .rule("ink")
       .test(/.*\.ink$/)
-      .use('ink-loader')
-      .loader(path.resolve('src/ink-loader.js'))
+      .use("ink-loader")
+      .loader(path.resolve("src/ink-loader.js"))
       .options({
-        cmd:
-          'mono /home/jeremie/Applications/inklecate_windows_and_linux/inklecate.exe'
+        cmd: "mono $INKLECATE"
       })
       .end();
   }
